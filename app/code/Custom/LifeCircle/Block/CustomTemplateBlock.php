1d523445974ae389e11c91ea43fa34b1055cd149
Quang Phi <xx.quangphi.2.9@gmail.com>
2023-11-09 15:11:37 +0700

    custom desc template

---
+++ app/code/Custom/LifeCircle/Block/CustomTemplateBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Custom\LifeCircle\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class CustomTemplateBlock extends Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
