44683a3745889cc5a23c46ff8f8d22ffab329e50
Quang Phi <xx.quangphi.2.9@gmail.com>
2023-11-16 14:36:19 +0700

    create module to filter customer with attr

---
+++ app/code/UnitFive/Customer/Block/CustomerList.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace UnitFive\Customer\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+
+class CustomerList extends Template
+{
+    protected $_customerRepository;
+    protected $_searchCriteriaBuilder;
+
+    public function __construct(
+        Context $context,
+        CustomerRepositoryInterface $customerRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        array $data = []
+    ) {
+        $this->_customerRepository = $customerRepository;
+        $this->_searchCriteriaBuilder = $searchCriteriaBuilder;
+        parent::__construct($context, $data);
+    }
+
+    public function getCustomerList()
+    {
+        $customerList = $this->_customerRepository->getList($this->getSearchCriteria());
+        $objType = get_class($customerList);
+        return [$customerList, $objType];
+    }
+
+    private function getSearchCriteria()
+    {
+        $builder = $this->_searchCriteriaBuilder
+            ->addFilter('email', 'roni_cost@example.com')
+            ->addFilter('firstname', 'Veronica', 'like')
+            ->create();
+        return $builder;
+    }
+}
---
+++ app/code/UnitFive/Customer/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'UnitFive_Customer',
+    __DIR__
+);
